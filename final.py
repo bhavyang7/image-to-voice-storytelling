69d3720cdb27839e42f07dc754ada1bd0fa2bf0d
bhavyang7 <39518761+bhavyang7@users.noreply.github.com>
2024-06-02 12:19:44 -0400

    Update and rename final to final.py

--- final.py
+++ final.py
@@ -5,7 +5,6 @@ import os
 from transformers import pipeline
 
 
-
 load_dotenv(find_dotenv())
 HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
@@ -17,16 +16,6 @@ def img2txt(url):
 
     print(text)
     return text
-
-def text2speech(message):
-    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-    headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKEN}"}
-    payloads= {
-        "inputs": message
-    }
-    response = requests.post(API_URL, headers=headers, json=payloads)
-    with open('audio.flac', 'wb') as file:
-        file.write(response.content)
 
 def text2story(story):
     API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom"
@@ -41,6 +30,17 @@ def text2story(story):
 
     print(response.content)
     return response.content
+
+def text2speech(message):
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKEN}"}
+    payloads= {
+        "inputs": message
+    }
+    response = requests.post(API_URL, headers=headers, json=payloads)
+    with open('audio.flac', 'wb') as file:
+        file.write(response.content)
+
 
 
 bro = img2txt("okakbro.jpg")
